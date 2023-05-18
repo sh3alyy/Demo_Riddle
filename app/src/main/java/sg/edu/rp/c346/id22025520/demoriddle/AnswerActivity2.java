fff2b8f87f351afec5504b22038c0bfd572aeeb0
Shea Lim <shealim@example.com>
2023-05-18 20:37:43 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22025520/demoriddle/AnswerActivity2.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id22025520.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnswerActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer2);
+    }
+}
